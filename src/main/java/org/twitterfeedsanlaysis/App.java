60fedb5c52f321fa339ee54de9839de059b8d0c1
uMklami <m.aamir.it@gmail.com>
2018-03-06 01:50:11 +0800

    File updated

--- src/main/java/org/twitterfeedsanlaysis/App.java
+++ src/main/java/org/twitterfeedsanlaysis/App.java
@@ -1,15 +1,25 @@
 package org.twitterfeedsanlaysis;
 
+import org.twitterfeedsanlaysis.TermOntologyMatcher;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static Logger log = LogManager.getRootLogger();
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("Hello Amir!");
-        System.out.println("Dr. Nazri with us!");
+    	log.info("Twitter Feed Anaylsis...");
+//    	if (args.length != 2) {
+//			log.error("Please provide two arguments, e.g tweets.txt ontologies.json");
+//			System.exit(0);
+//		}
+    	
+		log.info("Initializing ontologies!");
+		(new TermOntologyMatcher(args[0], args[1])).matchTerms();
+		log.info("Done!");
     }
 }
